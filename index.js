d17d08bc6ce5ff0484577e5e123c08237a6f2966
ngachadavid <davidngacha01@gmail.com>
2022-11-25 11:51:54 +0300

    Done with assinment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+const headQuarters = 42
+
+function distanceFromHqInBlocks(pickUp) {
+    if (pickUp > headQuarters) {
+        return pickUp - headQuarters;
+    } else {
+        return headQuarters - pickUp
+    }
+}
+
+function distanceFromHqInFeet(pickUp) {
+    return distanceFromHqInBlocks(pickUp) * 264;
+}
+
+
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+        return (destination - start)* 264
+    } else {
+        return (start - destination)* 264;
+    }
+}
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return 0.02 * (distance - 400);
+    }else if (distance > 2000 && distance < 2500) {
+        return 25;
+    }else {
+        return 'cannot travel that far';
+    }
+}
